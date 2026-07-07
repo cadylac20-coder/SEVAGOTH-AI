777914f8c6a9cb64baaef6839e2d1c11a0593cce
cadylac20-coder <cadylac20@gmail.com>
2026-07-07 12:38:43 +0530

    Delete modules directory

--- modules/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-SEVAGOTH Modules Package
-Contains all modularized features of SEVAGOTH
-"""
-
-# This file makes the modules folder a Python package
-# It can be empty, but imports can be added here if needed
